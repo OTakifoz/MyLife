e1c5652ca107f6fa14642dafa9ed131ddc8a9a85
OTakifoz <111560934+OTakifoz@users.noreply.github.com>
2023-08-02 17:30:14 +0300

    File organization*2*

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_life/firebase_options.dart';
-import 'package:my_life/splash_screen.dart';
-import 'package:my_life/widgets/authentication.dart';
+import 'package:my_life/widgets/authentication_screen/firebase_options.dart';
+import 'package:my_life/widgets/authentication_screen/splash_screen.dart';
+import 'package:my_life/widgets/authentication_screen/authentication_screen.dart';
 import 'package:my_life/widgets/start_screen/start_screen.dart';
 
 void main() async {
